b459ca5f833f59eb5feec9f88eb1a4e19df4c21a
Stevie.wintec <steber05@student.wintec.ac.nz>
2019-02-18 21:16:54 +1300

    Commit

---
+++ GUI/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box1 = new System.Windows.Forms.TextBox();
+            this.box2 = new System.Windows.Forms.TextBox();
+            this.check = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // box1
+            // 
+            this.box1.Location = new System.Drawing.Point(67, 23);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(100, 20);
+            this.box1.TabIndex = 0;
+            this.box1.Text = "0";
+            this.box1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // box2
+            // 
+            this.box2.Location = new System.Drawing.Point(259, 23);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(100, 20);
+            this.box2.TabIndex = 1;
+            this.box2.Text = "0";
+            this.box2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(175, 159);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(75, 23);
+            this.check.TabIndex = 2;
+            this.check.Text = "Check";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // result
+            // 
+            this.result.AcceptsReturn = true;
+            this.result.Location = new System.Drawing.Point(67, 49);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.result.Size = new System.Drawing.Size(292, 80);
+            this.result.TabIndex = 3;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(139, 132);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(144, 13);
+            this.label.TabIndex = 4;
+            this.label.Text = "Enter a number higher than 1";
+            this.label.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 183);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox box1;
+        private System.Windows.Forms.TextBox box2;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ GUI/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            label.Visible = false;
+            result.Clear();
+            int num1 = Convert.ToInt32(box1.Text);
+            int num2 = Convert.ToInt32(box2.Text);
+            LinkedList<int> nums = new LinkedList<int>();
+            LinkedList<int> primes = new LinkedList<int>();
+            //int lineCount = 0;
+
+            /*if (num1 > 1)
+            {
+                for (int i = num1; i < num2 + 1; i++)
+                {
+                    nums.AddLast(i);
+                }
+
+                foreach (var num in nums)
+                {
+                    if (primeNumber(num))
+                    {
+                        primes.AddLast(num);
+                    }
+                }
+
+                foreach (var p in primes)
+                {
+                    if (lineCount == 5)
+                    {
+                        result.AppendText("\r\n");
+                        lineCount = 0;
+                    }
+                    lineCount++;
+                    result.AppendText(Convert.ToString(p + " "));
+                }
+            }
+            else
+            {
+                label.Visible = true;
+            }*/
+
+            //sort if num1 is bigger than num2 or vice versa
+            if (num1 < num2)
+            {
+                for (int i = num1; i <= num2; i++)
+                {
+                    nums.AddLast(i);
+                }
+            }
+            else
+            {
+                for (int i = num2; i <= num1; i++)
+                {
+                    nums.AddLast(i);
+                }
+            }
+            
+            //sorts numbers from a list to another primes list
+            /*foreach (var num in nums)
+            {
+                if (primeNumber(num) && num > 1)
+                {
+                    primes.AddLast(num);
+                }
+            }*/
+
+            primes = sortPrimes(nums);
+
+            //Displays all prime numbers to third text box with a linesize of 5 numbers
+            /*foreach (var p in primes)
+            {
+                if (lineCount == 5)
+                {
+                    result.AppendText("\r\n");
+                    lineCount = 0;
+                }
+                lineCount++;
+                result.AppendText(Convert.ToString(p + " "));
+            }*/
+
+            displayPrimes(primes);
+
+
+        }
+
+        public bool primeNumber(int n)
+        {
+            for(int i = 2; i < n; i++)
+            {
+                if ((n % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public LinkedList<int> sortPrimes(LinkedList<int> nums)
+        {
+            LinkedList<int> lst = new LinkedList<int>();
+            foreach (var num in nums)
+            {
+                if (primeNumber(num) && num > 1)
+                {
+                    lst.AddLast(num);
+                }
+            }
+            return lst;
+        }
+
+        public void displayPrimes(LinkedList<int> primes)
+        {
+            int lineCount = 0;
+
+            foreach (var p in primes)
+            {
+                if (lineCount == 5)
+                {
+                    result.AppendText("\r\n");
+                    lineCount = 0;
+                }
+                lineCount++;
+                result.AppendText(Convert.ToString(p + " "));
+            }
+        }
+    }
+}
